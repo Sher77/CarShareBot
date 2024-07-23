d45afca8b177741cc12972f2fdc8a1abe7ddd889
Zaripov Sherzat <zaripovsherzat@mail.ru>
2024-07-23 14:08:15 +0500

    change url

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 require('dotenv').config();
-const express = require('express');
 const { Bot } = require('grammy');
 const { connectToDb, clearReservations } = require('./database/db');
 const cron = require('node-cron');
 const { startBot } = require('./bot');
-
-const app = express();
-
-const bot = new Bot(process.env.BOT_API_TOKEN);
-
-app.use(express.json());
-
-app.post('/webhook', (req, res) => {
-  bot.handleUpdate(req.body);
-  res.sendStatus(200);
-});
-
-const setWebhook = async () => {
-  try {
-    const webhookUrl = `https://${process.env.RAILWAY_STATIC_URL}/webhook`;
-    await bot.api.setWebhook(webhookUrl);
-    console.log('Webhook установлен:', webhookUrl);
-  } catch (error) {
-    console.error('Ошибка установки вебхука:', error);
-  }
-};
 
 const start = async () => {
   try {
     await connectToDb('CarShareBot');
     console.log('Успешно подключено!');
 
+    const bot = new Bot(process.env.BOT_API_TOKEN);
+
     startBot(bot);
 
-    await setWebhook();
+    const webhookUrl = 'https://car-share-bot.railway.app/webhook';
+
+    console.log('Попытка установить вебхук на URL:', webhookUrl);
+
+    try {
+      await bot.api.setWebhook(webhookUrl);
+      console.log('Webhook установлен:', webhookUrl);
+    } catch (error) {
+      console.error('Ошибка установки вебхука:', error);
+    }
 
     cron.schedule(
       '0 0 * * *',
@@ -49,10 +38,6 @@ const start = async () => {
         timezone: 'Asia/Almaty',
       }
     );
-
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Сервер запущен на порту ${process.env.PORT || 3000}`);
-    });
   } catch (err) {
     console.error('Ошибка при запуске: ', err);
   }
