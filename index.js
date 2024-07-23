ea8e1b031e4a00ddd7b50ea368dd0e011e488ad4
Zaripov Sherzat <zaripovsherzat@mail.ru>
2024-07-23 13:53:17 +0500

    railway

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 require('dotenv').config();
-const express = require('express');
+
 const { Bot } = require('grammy');
 const { connectToDb, clearReservations } = require('./database/db');
 const cron = require('node-cron');
 const { startBot } = require('./bot');
 
-const app = express();
-
-const bot = new Bot(process.env.BOT_API_TOKEN);
-
-app.use(express.json());
-app.post('/webhook', (req, res) => {
-  bot.handleUpdate(req.body);
-  res.sendStatus(200);
-});
-
-module.exports = app;
-
-(async () => {
+const start = async () => {
   try {
     await connectToDb('CarShareBot');
     console.log('Успешно подключено!');
+
+    const bot = new Bot(process.env.BOT_API_TOKEN);
 
     startBot(bot);
 
@@ -41,4 +31,6 @@ module.exports = app;
   } catch (err) {
     console.error('Ошибка при запуске: ', err);
   }
-})();
+};
+
+start().catch(console.error);
